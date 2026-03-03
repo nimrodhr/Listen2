3d785d58e60f5cdbdcdc10a1e60bccb0d817dc7f
nimrodhr <nimrodhr@gmail.com>
2026-03-04 00:58:25 +0200

    Initial Commit

---
+++ LSTN2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LSTN2
+//
+//  Created by Nimrod Hershkovitz on 04/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LSTN2/LSTN2App.swift
@@ -0,0 +1,17 @@
+//
+//  LSTN2App.swift
+//  LSTN2
+//
+//  Created by Nimrod Hershkovitz on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct LSTN2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
